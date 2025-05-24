786b9dab95d91a63cc7655018e6114ae1745e617
noahpaige <noahlandonpaige@gmail.com>
2025-05-23 18:37:51 -0700

    Update home-page.tsx

--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -6,7 +6,7 @@ import { useScroll } from "framer-motion";
 import WelcomeSection from "@/components/pages/home-page/sections/welcome-section";
 import HomePageSection from "@/components/pages/home-page/sections/home-page-section";
 import BottomNav from "@/components/pages/home-page/bottom-nav";
-import AnimatedBackground from "@/components/animated-background/animated-background2";
+import AnimatedBackground from "@/components/animated-background";
 import ClientOnly from "@/components/client-only";
 import { useScrollSections } from "@/components/pages/home-page/hooks/use-scroll-sections";
 import { Home, Folder, Info, Mail } from "lucide-react";
@@ -14,8 +14,8 @@ import { useSmoothWheelScroll } from "@/components/pages/home-page/hooks/use-smo
 
 const SECTIONS = new Map([
   ["Home", { component: WelcomeSection, icon: Home }],
-  ["Projects", { component: () => <div>Projects</div>, icon: Folder }],
   ["About", { component: () => <div>About</div>, icon: Info }],
+  ["Projects", { component: () => <div>Projects</div>, icon: Folder }],
   ["Contact", { component: () => <div>Contact</div>, icon: Mail }],
 ]);
 
