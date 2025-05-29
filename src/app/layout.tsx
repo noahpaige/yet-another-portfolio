476622669d86957239ca935c5a60750f4ee602ef
noahpaige <noahlandonpaige@gmail.com>
2025-05-29 11:44:20 -0700

    fix white space on mobile attempt 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
       className={` ${spaceMono.className} ${ibmPlexMono.className} ${lexendDeca.className}`}
     >
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
